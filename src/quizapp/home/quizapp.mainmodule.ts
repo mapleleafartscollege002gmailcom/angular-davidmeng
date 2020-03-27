eb062c9123de4ad67de3b34f4aa5bdab2452b0d4
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-03-26 22:38:06 -0400

    2 files modified

--- src/quizapp/home/quizapp.mainmodule.ts
+++ src/quizapp/home/quizapp.mainmodule.ts
@@ -6,6 +6,8 @@ import { MainRoutes } from '../routing/quizapp.mainrouting';
 import {RouterModule} from "@angular/router"
 import { MasterPageComponent } from './quizapp.masterpagecomponent';
 import { HomeComponent } from './quizapp.homecomponent';
+import { ques1component } from '../ques1/quizapp.ques1component';
+import { ques2component } from '../ques2/quizapp.ques2component';
 
 @NgModule({
   imports:      [
--- src/quizapp/routing/quizapp.mainrouting.ts
+++ src/quizapp/routing/quizapp.mainrouting.ts
@@ -1,11 +1,12 @@
 
 import { HomeComponent } from '../home/quizapp.homecomponent';
+import { ques1component } from '../ques1/quizapp.ques1component';
+import { ques2component } from '../ques2/quizapp.ques2component';
 export const MainRoutes = [
     { path: 'home', component: HomeComponent },
-    { path: 'ques1', 
-    loadChildren: '../ques1/quizapp.ques1component#Ques1Module' },
-    { path: 'ques2', 
-    loadChildren: '../ques2/quizapp.ques2component#Ques2Module' },
-    { path: '', component: HomeComponent }
+    { path: 'ques1', component: ques1component },
+    { path: 'ques2', component: ques2component },
+    { path: '', component: HomeComponent },
+    { path: '**', component: HomeComponent }
     
 ];

d05e99a5aa6f09ab351c4e05d62e22ff17c3fd66
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-03-27 09:44:59 -0400

    3 files modified

--- src/quizapp/ques2/quizapp.ques2component.ts
+++ src/quizapp/ques2/quizapp.ques2component.ts
@@ -2,11 +2,11 @@ import { Component, Input } from '@angular/core';
 
 import { HomeComponent } from '../home/quizapp.homecomponent';
 @Component({ 
+  selector: 'Hello',
   templateUrl: './quizapp.ques2view.html',
   styles: [`h1 { font-family: Lato; }`]
 })
 export class ques2component  {
   @Input() name: string;
 
-  @input ('parentdata') public name;
 }

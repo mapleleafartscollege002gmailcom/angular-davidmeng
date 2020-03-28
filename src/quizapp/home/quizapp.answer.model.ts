327b81eda386ef9feefbb88ed39d643e8a9595aa
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-03-27 20:15:48 -0400

    1 file modified

--- src/quizapp/home/quizapp.answer.model.ts
+++ src/quizapp/home/quizapp.answer.model.ts
@@ -1,9 +1,4 @@
 
 export class Answer{
-    Answer1:string = "";
-    Answer2:string = "";
-    Answer3:string = "";
-    Answer4:string = "";
-    Answer5:string = "";
-    constructor(){};
+    AnswerDescription:string = "";
 }

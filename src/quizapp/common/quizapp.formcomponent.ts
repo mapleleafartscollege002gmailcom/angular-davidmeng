98ef2b0457c3246e1c679ceacaffb21e3452514a
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-03-27 20:13:22 -0400

    5 files added, 6 deleted

---
+++ src/quizapp/common/quizapp.formcomponent.ts
@@ -0,0 +1,32 @@
+import {Component,
+    Input,
+    Output,
+    EventEmitter} from "@angular/core"
+
+@Component({
+    selector: "form-ui",
+    templateUrl : "./quizapp.formview.html"
+})
+export class FormComponent{
+  
+    questions: Array<Object> = new Array<Object>();
+    answers: Array<Object> = new Array<Object>();
+
+    @Input("form-questions")
+    set setQuestions(_questions:Array<Object>){
+       this.questions = _questions;     
+    }
+    @Input("form-answers")
+    set setAnswers(_answers:Array<Object>){
+        this.answers = _answers;     
+     }
+     
+     @Output("answer-selected")
+     eventemitter: EventEmitter<Object> = 
+     new EventEmitter<Object>();
+
+     SelectAnswer(_selected:Object){
+         this.eventemitter.emit(_selected);
+     }
+     
+}

90ecf64c9a3adc522d98e565d62b87ce67abc4ac
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-03-27 21:53:50 -0400

    1 file modified

--- src/quizapp/question/quizapp.questionmodule.ts
+++ src/quizapp/question/quizapp.questionmodule.ts
@@ -5,8 +5,6 @@ import {FormsModule , ReactiveFormsModule} from "@angular/forms"
 import {RouterModule} from "@angular/router"
 import {questioncomponent } from './quizapp.questioncomponent';
 import {FormComponent} from "../common/quizapp.formcomponent"
-import {HttpModule} from "@angular/http"
-import {HttpClientModule,HTTP_INTERCEPTORS} from "@angular/common/http"
 import {QuestionRoutes } from '../routing/quizapp.questionrouting';
 
 @NgModule({  
@@ -18,11 +16,7 @@ import {QuestionRoutes } from '../routing/quizapp.questionrouting';
     RouterModule.forChild(QuestionRoutes),
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpModule ,
-    HttpClientModule
-  ],
-  exports: [ 
+    ReactiveFormsModule
   ],
   providers: [ 
   ],

21f8114a73cf91b8e503de37eb2fda46ad3745dc
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-04-01 22:27:07 -0400

    1 file modified

--- src/quizapp/routing/quizapp.mainrouting.ts
+++ src/quizapp/routing/quizapp.mainrouting.ts
@@ -4,6 +4,8 @@ export const MainRoutes = [
     { path: 'home', component: HomeComponent },
     { path: 'question', 
     loadChildren: '../question/quizapp.questionmodule#questionmodule' },
+    { path: 'contact', 
+    loadChildren: '../contact/quizapp.contactmodule#contactmodule' },
     { path: '', component: HomeComponent },
     { path: '**', component: HomeComponent }    
 ];

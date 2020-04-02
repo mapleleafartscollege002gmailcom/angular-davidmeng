ee721d82830649244f2555dc3160d716a25763b5
mapleleafartscollege002gmailcom <mapleleafartscollege002@gmail.com>
2020-04-01 20:39:34 -0400

    2 files modified

--- src/quizapp/common/quizapp.itemcomponent.ts
+++ src/quizapp/common/quizapp.itemcomponent.ts
@@ -18,28 +18,23 @@ export class ItemComponent{
     
     @Input("form-questions")
     set setQuestions(_questions:Array<Question>){
-       this.questions = _questions;    
-    this.question = new Question();
-       if(this.answers.length >= 0 && this.answers.length <5)
-        this.question = this.questions[this.answers.length];
+      this.questions = _questions;    
+      this.question = new Question();
+      this.question = this.questions[this.answers.length];
     }
     
     @Input("form-answers")
     set setAnswers(_answers:Array<Object>){
-        this.answers = _answers;    
-    this.question = new Question();
-       if(this.answers.length >= 0 && this.answers.length <5)
-        this.question = this.questions[this.answers.length];
+      this.answers = _answers;    
+      this.question = new Question();
+      this.question = this.questions[this.answers.length];
     }
     
     @Output("answer-selected")
     eventemitter: EventEmitter<Object> = new EventEmitter<Object>();
     SelectAnswer(){
-      //alert(123);
-       this.eventemitter.emit(this.selectAnswer);
-       // this.answers.push(this.selectAnswer);
-    this.question = new Question(); 
-       if(this.answers.length >= 0 && this.answers.length <5)
-        this.question = this.questions[this.answers.length];
+      this.eventemitter.emit(this.selectAnswer);
+      this.question = new Question(); 
+      this.question = this.questions[this.answers.length];
     }    
 }
